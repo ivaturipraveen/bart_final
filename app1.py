0c594bb99ff94fb74e9d039ff958d56a77b2210d
ivaturipraveen <113433003+ivaturipraveen@users.noreply.github.com>
2024-12-01 17:46:06 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -3,6 +3,7 @@ import json
 import numpy as np
 import faiss
 from sentence_transformers import SentenceTransformer, CrossEncoder
+import os
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -69,6 +70,7 @@ def handle_query():
 
     return jsonify(response)
 
-# Run the Flask app
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
+
